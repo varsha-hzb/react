f55d883790a53f4c2dffbe6e51736e7730d2cbe2
varsha <varshahzb@gmail.com>
2023-12-18 22:05:15 -0800

    start of react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Tea from "./tea"
+
+function App() {
+
+
+  return (
+    <>
+    <Tea/>
+    <h1>Lorem ipsum dolor sit amet.</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/tea.jsx
@@ -0,0 +1,7 @@
+function Tea(){
+    return(
+        <h3>react is awesome</h3>
+    )
+}
+
+export default Tea
